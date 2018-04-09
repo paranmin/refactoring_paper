5f7cf76f1cde5c2daf6a2542361631ebb693bbda
GangDae Lee <enhoomin@naver.com>
2018-04-09 11:23:35 +0900

    메서드 이동 적용
    Customer amountFor -> Rental getCharge

--- src/main/java/refactoring_paper/video/Customer.java
+++ src/main/java/refactoring_paper/video/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 
 		for (Rental each : rentals) {
-			double thisAmount = amountFor(each);
+			double thisAmount = each.getCharge();
 
 			frequentRenterPoints++;
 
@@ -44,28 +44,5 @@ public class Customer {
 		result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
 
 		return result.toString();
-	}
-
-	private double amountFor(Rental aRental) {
-		double result = 0;
-
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (aRental.getDaysRented() > 2) {
-				result += (aRental.getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHIDLENS:
-			result += 1.5;
-			if (aRental.getDaysRented() > 3) {
-				result += (aRental.getDaysRented() - 3) * 1.5;
-			}
-			break;
-		}
-		return result;
 	}
 }
