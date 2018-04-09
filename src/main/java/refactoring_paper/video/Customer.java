b5a11b9a6828d93d34614edcf564dcb68e45981c
GangDae Lee <enhoomin@naver.com>
2018-04-09 11:20:12 +0900

    메서드 추출 (switch -> amountFor)

--- src/main/java/refactoring_paper/video/Customer.java
+++ src/main/java/refactoring_paper/video/Customer.java
@@ -26,25 +26,7 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 
 		for (Rental each : rentals) {
-			double thisAmount = 0;
-
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2) {
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHIDLENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3) {
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				}
-				break;
-			}
+			double thisAmount = amountFor(each);
 
 			frequentRenterPoints++;
 
@@ -62,5 +44,28 @@ public class Customer {
 		result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
 
 		return result.toString();
+	}
+
+	private double amountFor(Rental aRental) {
+		double result = 0;
+
+		switch (aRental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (aRental.getDaysRented() > 2) {
+				result += (aRental.getDaysRented() - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result += aRental.getDaysRented() * 3;
+			break;
+		case Movie.CHIDLENS:
+			result += 1.5;
+			if (aRental.getDaysRented() > 3) {
+				result += (aRental.getDaysRented() - 3) * 1.5;
+			}
+			break;
+		}
+		return result;
 	}
 }
