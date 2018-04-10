36e64336b733fbcebe3ccd297d05ac1efa7ef5e3
GangDae Lee <enhoomin@naver.com>
2018-04-10 09:45:24 +0900

    메서드 이동 (Rental -> Moive)

--- src/main/java/refactoring_paper/video/Movie.java
+++ src/main/java/refactoring_paper/video/Movie.java
@@ -25,4 +25,34 @@ public class Movie {
 		return title;
 	}
 
+	double getCharge(int daysRented) {
+		double result = 0;
+	
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2) {
+				result += (daysRented - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHIDLENS:
+			result += 1.5;
+			if (daysRented > 3) {
+				result += (daysRented - 3) * 1.5;
+			}
+			break;
+		}
+		return result;
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+			return 2;
+		}
+		return 1;
+	}
+
 }
